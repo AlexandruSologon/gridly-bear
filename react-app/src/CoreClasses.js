b5546874bbb41ccfe700593337f7b880462c2737
Shashwat Sahay <S.Sahay-1@student.tudelft.nl>
2024-05-10 13:05:06 +0200

    Adds new file with core classes

---
+++ react-app/src/CoreClasses.js
@@ -0,0 +1,49 @@
+class Bus {
+    constructor(id, xcoord, ycoord) {
+        this.id = id
+        this.xcoord = xcoord
+        this.ycoord = ycoord
+    }
+
+    move(newx, newy) {
+        this.xcoord = newx
+        this.ycoord = newy
+    }
+}
+
+class Line {
+    constructor(bus1, bus2) {
+        this.bus1 = bus1
+        this.bus2 = bus2
+    }
+}
+
+class Transformer {
+    constructor(highBus, lowBus, type) {
+        this.highBus = highBus
+        this.lowBus = lowBus
+        this.type = type
+    }
+}
+
+class Switch {
+    constructor(from, to, type) {
+        this.from = from
+        this.to = to
+        this.type = type
+    }
+}
+
+class Load {
+    constructor(bus, power) {
+        this.bus = bus
+        this.power = power
+    }
+}
+
+class Generator extends Load{
+    constructor(bus, power) {
+        super()
+        this.power = - this.power
+    }
+}
