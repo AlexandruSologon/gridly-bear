28d7113dd5eb7106e38b703f46f05fc265138627
jtijssens <J.D.Tijsssens@student.tudelft.nl>
2024-05-27 13:29:53 +0200

    Altered the visual of the loading anular

--- react-app/src/runButton.js
+++ react-app/src/runButton.js
@@ -4,6 +4,11 @@ import IconButton from '@mui/material/IconButton';
 import Spinner from 'spin.js';
 import CircularProgress from '@mui/material/CircularProgress';
 
+/**
+ * Renders a green arrow run button.
+ * @param {*} onRunButtonClick - The function to be called on the button's click.
+ * @returns The jsx for the run button.
+ */
 function GreenArrow({onRunButtonClick}) {
     return (<IconButton aria-label="check" style={{
         position: 'absolute',
@@ -24,14 +29,40 @@ function GreenArrow({onRunButtonClick}) {
     </IconButton>);
 }
 
+/**
+ * Render a rotating waiting icon (aka an anular throbber).
+ * @returns the jsx content for rendering the loading icon.
+ */
 function AnularThrobber() {
     return (
-        <div style={{ position: 'absolute', right: '0px', top: '78%', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <CircularProgress style={{ color: '#05a95c' }} />
+        <div style={
+            { 
+                position: 'absolute', 
+                right: '0px', 
+                top: '78%', 
+                justifyContent: 'center', 
+                alignItems: 'center', 
+                height: '100vh'
+            }
+        }>
+            <CircularProgress style={
+                { 
+                    color: '#05a95c', 
+                    width: '8vw', 
+                    height: '8vw' 
+                }
+            } 
+            />
         </div>
     );
 }
 
+/**
+ * Renders both a run button and a loading icon (anular throbber) depending on the state of the canvas.
+ * @param {*} runClicked Boolean to convey the state of the canvas, "has this button been clicked and was there no response yet?".
+ * @param {*} onRunButtonClick The action to be taken by the run button.
+ * @returns 
+ */
 function runButton({runClicked, onRunButtonClick}) {
     if(runClicked) {
         return AnularThrobber();
