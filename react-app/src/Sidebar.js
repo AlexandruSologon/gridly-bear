19a749412c6050da19fe9f47a1a43ac7d31c75aa
jtijssens <J.D.Tijsssens@student.tudelft.nl>
2024-05-29 13:04:04 +0200

    seperated sidebar sub-components

--- react-app/src/Sidebar.js
+++ react-app/src/Sidebar.js
@@ -4,80 +4,99 @@ import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArro
 import React, {useState} from "react";
 import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
 
+function Draggables({state}) {
+    return (<div style={{ alignContent: 'center'}}>
+    {state.sidebarItems.map((item) => (
+        <div
+            key={item.id}
+            draggable={true}
+            onDragStart={(event) => state.handleDragStart(event, item)}
+            onDragEnd={state.handleDragEnd}
+            style={{margin: '10px ', cursor: 'grab',width: '96px', height: '96px'}}
+        >
+            {/* Container for icon and text */}
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                {/* Render the icon based on item.type */}
+                <img src={state.iconMapping[item.type].options.iconUrl} alt={item.name} style={{width: '64px', height: '64px'}}/>
+                {/* Render the text */}
+                <div>    {item.name}</div>
+            </div>
+        </div>
+    ))}
+    </div>);
+}
+
+function CollapseButton({onSidebarToggle, isSidebarOn}) {
+    return (
+        <IconButton 
+            style={{
+                position: 'absolute',
+                width: '30px',
+                height: '30px',
+                left: '100px',
+                top: '0px',
+                zIndex: 1001
+            }} 
+            onClick={onSidebarToggle}
+        >
+            <KeyboardDoubleArrowRightIcon 
+                className="KeyboardDoubleArrowRightIcon"
+                style={{
+                    display: !isSidebarOn ? 'flex' : 'none',
+                    width: '30px',
+                    height: '30px'
+                }}
+            />
+            <KeyboardDoubleArrowLeftIcon 
+                className="KeyboardDoubleArrowLeftIcon"
+                style={{
+                    display: isSidebarOn ? 'flex' : 'none',
+                    width: '30px',
+                    height: '30px'
+                }}
+            />
+        </IconButton>
+    );
+}
 
 function Sidebar(state) {
-        const [isSidebarOn, setIsSidebarOn] = useState(true);
+    const [isSidebarOn, setIsSidebarOn] = useState(true);
 
-
-
-        const onSidebarToggle = () => {
+    const onSidebarToggle = () => {
         setIsSidebarOn(!isSidebarOn)
     }
 
-return (<div>
-                <Drawer
-                    PaperProps={{
-                        sx: {
-                            backgroundColor: "#F9F6EE",
+    return (
+        <div>
+            <Drawer 
+                PaperProps={{
+                    sx: {
+                            backgroundColor: " #fdfdfd",
                             color: "#000",
-                            width: '150px'
-                        }
-                    }}
-                    style={{
-                        display: isSidebarOn ? 'grid' : 'none',
-                        position: 'absolute',
-                        zIndex: 1000,
-                    }}
-                    variant="permanent"
-                    anchor="bottomleft"
-                >
-                    <div style={{height: '3%'}}></div>
-                    <h2 style={{
-                        fontSize: '19px',
-                        fontFamily: 'Arial, sans-serif',
-                        overflow: 'auto'
-                    }}>
-                    </h2>
-                    {/* Render draggable items */}
-                    {state.sidebarItems.map((item) => (
-                        <div
-                            key={item.id}
-                            draggable={true}
-                            onDragStart={(event) => state.handleDragStart(event, item)}
-                            onDragEnd={state.handleDragEnd}
-                            style={{margin: '10px ', cursor: 'grab',width: '96px', height: '96px'}}
-                        >
-                            {/* Container for icon and text */}
-                                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                                {/* Render the icon based on item.type */}
-                                <img src={state.iconMapping[item.type].options.iconUrl} alt={item.name} style={{width: '64px', height: '64px'}}/>
-                                {/* Render the text */}
-                                <div>    {item.name}</div>
-                            </div>
-                        </div>
-                    ))}
-                </Drawer>
-                <IconButton style={{
+                            width: '150px',
+                            textAlign: 'center'
+                        }}}
+                style={{
+                    display: isSidebarOn ? 'grid' : 'none',
                     position: 'absolute',
-                    width: '30px',
-                    height: '30px',
-                    left: '100px',
-                    top: '0px',
-                    zIndex: 1001
-                }} onClick={onSidebarToggle}>
-                    <KeyboardDoubleArrowRightIcon className="KeyboardDoubleArrowRightIcon"
-                                                  style={{
-                                                      display: !isSidebarOn ? 'flex' : 'none',
-                                                      width: '30px',
-                                                      height: '30px'
-                                                  }}/>
-                    <KeyboardDoubleArrowLeftIcon className="KeyboardDoubleArrowLeftIcon"
-                                                  style={{
-                                                      display: isSidebarOn ? 'flex' : 'none',
-                                                      width: '30px',
-                                                      height: '30px'
-                                                  }}/>
-                </IconButton>
-            </div>)}
+                    zIndex: 1000,
+                }}
+                variant="permanent"
+                anchor="bottomleft"
+            >
+                <div style={{height: '3%'}}></div>
+                <h2 style={{
+                        fontSize: '16px', // Increased font size
+                        fontFamily: 'Helvetica, sans-serif', // Changed font family
+                        margin: '10px 0', // Added margin for spacing
+                    }}
+                >
+                    Drag & Drop
+                </h2>
+                <Draggables state={state}></Draggables>
+            </Drawer>
+            <CollapseButton onSidebarToggle={onSidebarToggle} isSidebarOn={isSidebarOn}></CollapseButton>
+        </div>);
+}
 
 export default Sidebar;
