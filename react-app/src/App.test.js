fdc516d7081499ea626a21dc384f6043845c1f68
Alexandru <a.m.sologon@student.tudelft.nl>
2024-05-30 14:16:59 +0200

    testing

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -1,5 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+//
+import { TextEncoder, TextDecoder } from 'util'
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+  jest.mock('react-leaflet', () => jest.fn());
+  jest.mock('firebase-functions', () => jest.fn());
 
 test('renders learn react link', () => {
   render(<App />);
