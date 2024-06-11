4f30b8708d93b6f806ae0d2499d8fd1da200dea4
jtijssens <J.D.Tijsssens@student.tudelft.nl>
2024-06-11 09:35:16 +0200

    Lock button tooltip set to lock map instead of input

--- react-app/src/interface-elements/LockButton.js
+++ react-app/src/interface-elements/LockButton.js
@@ -13,7 +13,7 @@ function LockButton(props) {
     }
 
     return(
-        <Tooltip title="import">
+        <Tooltip title="lock map">
             <Button data-testid = "lockbutton" className={'hasShadow'} size={'large'} onClick={onLockButtonClick} type="default" shape="square"
                     icon={!isMapLocked ? <LockIcon data-testid = "lock-close-icon"/> : <LockOpenIcon data-testid = "lock-open-icon"/>}>
             </Button>
