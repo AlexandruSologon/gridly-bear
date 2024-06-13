c5635b2a74de1b9bf7ab2861e85b0ca58bb53693
Alexandru <a.m.sologon@student.tudelft.nl>
2024-06-13 13:27:33 +0200

    tests for net.py

--- react-app/functions/test/api_test.py
+++ react-app/functions/test/api_test.py
@@ -1,18 +1,26 @@
-import pandapower as pp
-from ..src import net, jsonParser
 import unittest
-import json
+from .. import main
+
+
+def test_network():
+    return ('{"data":{"components":[{"class":"bus","id":0,"pos":{"lat":51.90869633027845,"lng":4.407817839528435},'
+            '"voltage":20},{"class":"bus","id":1,"pos":{"lat":51.90774321365463,"lng":4.458972929860466},'
+            '"voltage":0.4},{"id":0,"class":"generator","bus":0,"p_mw":5, "vm_pu": null},{"id":0,"class":"load",'
+            '"bus":1,"p_mv":5,"q_mvar":5},{"id":0,"class":"line","type":"NAYY 4x50 SE","bus1":0,"bus2":1,'
+            '"length":5}]}}')
+
 
 class TestAPIUsage(unittest.TestCase):
 
     def test3(self):
-        pass
+        print(main.cnvs_json_post(test_network()))
 
     def test2(self):
         pass
-    
+
     def test1(self):
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
