98a9f3201db2c65653cd26cea636466d6fedf237
jtijssens <J.D.Tijsssens@student.tudelft.nl>
2024-06-18 18:30:39 +0200

    Made functions deployable again

--- react-app/functions/main.py
+++ react-app/functions/main.py
@@ -1,12 +1,21 @@
 from typing import Any
 import sys
 import os
+import json
 from firebase_admin import initialize_app
 from firebase_functions import https_fn, options
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src import apiHandler
+#comment
 
 app = initialize_app()
+
+# Hello world example function, do not use in deployment
+@https_fn.on_request(cors=options.CorsOptions(
+    cors_origins="*",
+    cors_methods=["get", "post", "options"], ))
+def hello_world() -> str:
+    return json.dumps({"data": "hello!"})
 
 # Handles the posting of json data corresponding to the canvas state
 @https_fn.on_request(memory=2048,
