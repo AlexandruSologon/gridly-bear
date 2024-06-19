c013e81270f9b97035ce971521775dd518a2bc18
jtijssens <J.D.Tijsssens@student.tudelft.nl>
2024-06-19 10:15:21 +0200

    Remove extra closing brackets

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -490,7 +490,7 @@ export function ReactApp() {
                                         handleParameterChange={handleParameterChange}
                                         handleMarkerDelete={handleMarkerDelete}
                                         handleTransReverse={handleTransReverse}
-                                        replaceDefaultValues = {replaceDefaultValues}/>/>
+                                        replaceDefaultValues = {replaceDefaultValues}/>
                                 </Marker>))}
                             {lines.map((line, index) => (
                                 <Polyline key={index}
@@ -499,7 +499,7 @@ export function ReactApp() {
                                           pathOptions={{color: line.color}}
                                           positions={[line.position1, line.position2]}
                                           ref={(ref) => (lineRefs.current[index] = ref)}
-                                          renderer={L.canvas({tolerance:15})}
+                                          renderer={L.canvas({padding: 0.5,tolerance:15})}
                                           eventHandlers={{
                                               click: (e) => handleLineClick(e),
                                               contextmenu: (e) => handleLineRightClick(e)
