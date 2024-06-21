604422166f5ce69cfe6751dc6520b0eb3738f957
jtijssens <J.D.Tijsssens@student.tudelft.nl>
2024-06-21 11:06:03 +0200

    Added content to info panel

--- react-app/src/interface-elements/InfoPanel.js
+++ react-app/src/interface-elements/InfoPanel.js
@@ -1,5 +1,8 @@
-import { Modal } from "antd";
+import { Divider, Modal } from "antd";
 import {Carousel, Image} from "antd";
+import dragdroptutorial from '../images/dragdroptutorial.gif';
+import makelinetutorial from '../images/makelinetutorial.gif';
+import connectwithbuses from '../images/connectwithbuses.png';
 
 export default function InfoPanel({ active, setInfoActive }) {
     return (
@@ -19,27 +22,46 @@ export default function InfoPanel({ active, setInfoActive }) {
 }
 
 function CarouselFilled() {
-
     const contentStyle = {
-        margin: 100,
-        height: '500px',
+        margin: '2%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center', // Add this line to center vertically
         color: 'grey',
-        lineHeight: '160px',
+        lineHeight: '10px',
         textAlign: 'center',
-        background: '#black',
     };
 
     return (
-        <Carousel arrows infinite={false} style={{width:'100%', height:'100%'}}>
+        <Carousel arrows infinite={false} style={{ width: '100%', height: '100%' }}>
+
             <div style={contentStyle}>
-                <h1 style={contentStyle}>ola</h1>
+                <h3 style={{margin:'3%'}}>Drag and drop items</h3>
+                <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                    <Image preview={false} width="70%" src={dragdroptutorial} />
+                    <Divider type="vertical"></Divider>
+                    <p>Hold and drag from the sidebar to put network components on the map</p>
+                </div>
             </div>
+
             <div style={contentStyle}>
-                <h1 style={contentStyle}>ola</h1>
+                <>
+                <h3>Create lines between two components</h3>
+                <Image preview={false} width="30%" src={makelinetutorial} />
+                <Divider type="vertical"/>
+                <p>hi</p>
+                </>
             </div>
+
             <div style={contentStyle}>
-                <h1 style={contentStyle}>ola</h1>
+                <>
+                <h3>Connect items using the bus component</h3>
+                <Image preview={false} width="40%" src={connectwithbuses} />
+                <Divider type="vertical"/>
+                <p>hi</p>
+                </>
             </div>
+
         </Carousel>
-    )
+    );
 }
