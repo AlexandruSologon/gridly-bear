4c267eb137a0bb880d300196beb881a84c3665bc
jtijssens <J.D.Tijsssens@student.tudelft.nl>
2024-06-21 13:31:06 +0200

    info panel extended

--- react-app/src/interface-elements/InfoPanel.js
+++ react-app/src/interface-elements/InfoPanel.js
@@ -2,6 +2,8 @@ import { Divider, Modal, Carousel, Image } from "antd";
 import dragdroptutorial from '../images/dragdroptutorial.gif';
 import makelinetutorial from '../images/makelinetutorial.gif';
 import connectwithbuses from '../images/connectwithbuses.png';
+import runbuttonfull from '../images/runbuttonfull.png';
+import settingstutorial from '../images/settingstutorial.gif';
 
 export default function InfoPanel({ active, setInfoActive }) {
     return (
@@ -13,7 +15,7 @@ export default function InfoPanel({ active, setInfoActive }) {
             width={'60vw'}
             footer={[]}
         >
-            <div style={{height: '60vh'}}>
+            <div style={{height: '60vh', overflow:'hidden'}}>
                 <CarouselFilled></CarouselFilled>
             </div>
         </Modal>
@@ -38,6 +40,7 @@ function CarouselFilled() {
         justifyContent: 'center', // Center content vertically
         alignItems: 'center', // Center content horizontally
         margin: '3%',
+        overflow:'hidden'
     };
 
     return (
@@ -58,7 +61,7 @@ function CarouselFilled() {
                     <Image preview={false} width="30%" src={makelinetutorial} />
                     <Divider type="vertical"/>
                     <div style={{display:'flex', flexDirection: 'column'}}>
-                        <p>Click on a component to select it</p>
+                        <p>Left click on a component to select it</p>
                         <p>Next, click on a compatible component to create a connection between the two</p>
                     </div>
                 </div>
@@ -70,10 +73,36 @@ function CarouselFilled() {
                     <Image preview={false} width="40%" src={connectwithbuses} />
                     <Divider type="vertical"/>
                     <div style={{display:'flex', flexDirection: 'column'}}>
-                        <p>Certain elements need to connect through a bus</p>
-                        <p>Blue lines indicate direct connections</p>
+                        <p>Elements can only connect through a bus</p>
+                        <p>Blue lines indicate direct connections and do not denote physical lines</p>
                         <p>Grey and Colorful lines indicate physical lines, these are only found between two buses</p>
-                        <p>yeah</p>
+                    </div>
+                </div>
+            </div>
+
+            <div style={contentStyle}>
+                <h3 style={{margin:'3%'}}>Enter parameters for components</h3>
+                <div style={{display:'flex', justifyContent:'center', alignItems:'center', margin:'5%', paddingTop:'5%'}}>
+                    <Image preview={false} width="40%" src={settingstutorial} />
+                    <Divider type="vertical"/>
+                    <div style={{display:'flex', flexDirection: 'column'}}>
+                        <p>right click elements to enter parameters for them</p>
+                        <p>'Set as default' to save the current parameters for any new component of the same type</p>
+                        <p>Make sure not to leave any parameters empty in order to run your simulation</p>
+                    </div>
+                </div>
+            </div>
+
+            <div style={contentStyle}>
+                <h3 style={{margin:'3%'}}>Run a simulation</h3>
+                <div style={{display:'flex', justifyContent:'center', alignItems:'center', margin:'5%', paddingTop:'5%'}}>
+                    <Image preview={false} width="40%" src={runbuttonfull} />
+                    <Divider type="vertical"/>
+                    <div style={{display:'flex', flexDirection: 'column'}}>
+                        <p>To run a simulation on your network hit 'run'</p>
+                        <p>Colors and numbers will indicate the diagnostic status of your components if the simulation was successful</p>
+                        <p>If your network did not run because it was invalid, have a look at the alert message at the top of the screen</p>
+                        <p>You can view a history of your run simulations and click them to set the canvas back to a previous network</p>
                     </div>
                 </div>
             </div>
